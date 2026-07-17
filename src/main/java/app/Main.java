5343f2802466bbe000f5ac110e42ba42976cae4a
ASHMIT RAJ <ashmitr0344@gmail.com>
2026-07-17 19:59:33 +0530

    Main part

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,20 @@
 package app;
 
+import ui.Dashboard;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: Implement application entry point
+        // Set cross-platform look and feel for consistent rounded border drawings
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            new Dashboard();
+        });
     }
 }
